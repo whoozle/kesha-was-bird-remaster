ce115610630b0cf8d5253d63528e683678fb9a43
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-01-21 00:22:08 +0000

    added minimal working example

--- generate-texture.py
+++ generate-texture.py
@@ -1,50 +1,112 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import argparse
 import png
+import operator
+import random
 
 parser = argparse.ArgumentParser(description='Compile font.')
 parser.add_argument('source', help='input file')
 parser.add_argument('name', help='name')
-parser.add_argument('planes', type=int, help='planes (1/2)')
-parser.add_argument('tile-size', type=int, help='tile size (8/16)')
+parser.add_argument('--compress', '-c', action='store_true', help='compress bitmap with lz4 algorithm')
+parser.add_argument('--algorithm', '-A', type=str, help='select algo', default='lz4')
+parser.add_argument('--palette', '-p', nargs='+', help='palette')
 args = parser.parse_args()
 
 tex = png.Reader(args.source)
 w, h, pixels, metadata = tex.read_flat()
-tile_size = getattr(args, 'tile-size')
-if tile_size != 8 and tile_size != 16:
-	raise Exception("invalid tile size %d" %tile_size)
-tw, th = tile_size, tile_size
-
-def label(name):
-	return ": tile_%s_%s" %(args.name, name)
-
+tw, th = 4, 4
 nx = (w + tw - 1) / tw
 ny = (h + th - 1) / th
+#data = bytearray([0] * (tw * th * nx * ny * 4 / 8))
+data = bytearray([0] * 0x1800)
+attrs = bytearray([0] * (nx * ny))
+palette = map(int, args.palette)
 
-def get_pixel(x, y, plane):
+def get_pixel(x, y):
 	if x < 0 or x >= w:
 		return 0
 	if y < 0 or y >= h:
 		return 0
 
-	bit = 1 << plane
-	return 1 if pixels[y * w + x] & bit else 0
+	return pixels[y * w + x]
 
-print label("data"),
-for ty in xrange(0, ny):
-	basey = ty * th
-	if nx > 1 or ny > 1:
-		print "\n" + label("row_%d" %ty)
-	for tx in xrange(0, nx):
-		basex = tx * tw
-		if nx > 1 or ny > 1:
-			print "\n" + label("%d_%d" %(ty, tx))
-		for plane in xrange(0, args.planes):
-			for y in xrange(0, th):
-				for x in xrange(0, tw / 8):
-					byte = 0
-					for bit in xrange(0, 8):
-						byte |= get_pixel(basex + x * 8 + bit, basey + y, plane) << (7 - bit)
-					print "0x%02x" %byte ,
+def set_pixel(x, y):
+	assert y < 128
+	bit = x & 7
+	x >>= 3
+	y2, y1, y0 = y >> 6, (y >> 3) & 7, y & 7
+	addr = (((y2 << 6) | (y0 << 3) | y1) << 5) | x
+	data[addr] |= 0x80 >> bit
+
+def set_pixel2x(x, y):
+	set_pixel(x * 2, y * 2)
+	set_pixel(x * 2 + 1, y * 2)
+	set_pixel(x * 2, y * 2 + 1)
+	set_pixel(x * 2 + 1, y * 2 + 1)
+
+if args.compress:
+	packed_data = bytearray()
+	if args.algorithm == "lz4":
+		from lz4.block import compress
+		compressed_data = bytearray(compress(packed_data, mode='high_compression', compression=12))
+	elif args.algorithm == "huffman":
+		compressed_data = huffman(packed_data)
+
+	print "#compressed size: %d of %d\n" %(len(compressed_data), len(packed_data))
+	print " ".join(map(lambda x: "0x%02x" %x, compressed_data))
+
+else:
+	for ty in xrange(ny):
+		basey = ty * th
+		for tx in xrange(nx):
+			basex = tx * tw
+			stats = {}
+			for y in xrange(th):
+				for x in xrange(tw):
+					v = get_pixel(basex + x, basey + y)
+					stats[v] = stats.setdefault(v, 0) + 1
+			stats = stats.items()
+			stats.sort(key=operator.itemgetter(1), reverse=True)
+			stats = stats[:2]
+			if len(stats) >= 2:
+				bg, fg = stats[0][0], stats[1][0]
+				attr = (palette[bg] << 3) | palette[fg]
+			else:
+				bg, fg = stats[0][0], 99
+				attr = (palette[bg] << 3)
+
+			attrs[ty * nx + tx] = attr
+
+			for yb in xrange(th):
+				y = basey + yb
+				for xb in xrange(tw):
+					x = basex + xb
+					v = get_pixel(x, y)
+					if fg == v:
+						set_pixel2x(x, y)
+					elif bg != v:
+						pass
+						#if v & 1:
+						#	set_pixel(x * 2, y * 2)
+						#	set_pixel(x * 2 + 1, y * 2 + 1)
+						#else:
+						#	set_pixel(x * 2 + 1, y * 2)
+						#	set_pixel(x * 2, y * 2 + 1)
+						#if random.randint(0, 1) == 0:
+						#	set_pixel(x * 2, y * 2)
+						#if random.randint(0, 1) == 0:
+						#	set_pixel(x * 2, y * 2 + 1)
+						#if random.randint(0, 1) == 0:
+						#	set_pixel(x * 2 + 1, y * 2)
+						#if random.randint(0, 1) == 0:
+						#	set_pixel(x * 2 + 1, y * 2 + 1)
+						#if v & 1:
+						#	set_pixel2x(x, y)
+						#	set_pixel(x * 2, y * 2)
+						#	set_pixel(x * 2 + 1, y * 2 + 1)
+
+	hexdata = ", ".join(map(hex, data + attrs))
+	#hexattrs = ", ".join(map(hex, attrs))
+	print "unsigned char %s_data[] = {%s};" %(args.name, hexdata)
+	#print "unsigned char %s_attrs[] = {%s};" %(args.name, hexattrs)
--- sources/main.c
+++ sources/main.c
@@ -1,8 +1,11 @@
 #include <string.h>
 
+//extern unsigned char splash_data[];
+
 int main() {
-	memset((unsigned char *)0x4000, 0x1, 0x1800);
-	memset((unsigned char *)0x5800, 0x07, 0x300);
+	unsigned char * src = (unsigned char *)0xda00;
+	memcpy((unsigned char *)0x4000, src, 0x1800);
+	memcpy((unsigned char *)0x5800, src + 0x1800, 0x200);
 
 	while(1);
 	return 0;
