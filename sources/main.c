fa829fe72ff5ee36f6a86600cebece0a6090c00f
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-01-22 20:46:20 +0000

    we don't need splash, it will go to loading screen

--- sources/main.c
+++ sources/main.c
@@ -1,12 +1,12 @@
 #include <string.h>
 #include "runtime.h"
-#include "texture_splash.h"
+#include "texture_memory_erizer.h"
 #include "lz4.h"
 
 void main() {
 	gsinit();
 
-	lz4_unpack((unsigned char *)0x4000, splash_data);
+	lz4_unpack((unsigned char *)0x4000, memory_erizer_data);
 	memset((unsigned char *)0x5800, 7, 0x300);
 
 	halt();
