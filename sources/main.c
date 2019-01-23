a5e9ca3652063a6f8650b04247eff9ea411bdc62
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-01-23 00:51:17 +0000

    added phone palette

--- sources/main.c
+++ sources/main.c
@@ -5,17 +5,18 @@
 #include "texture_professor.h"
 #include "texture_drinking.h"
 #include "texture_room.h"
+#include "texture_phone.h"
 #include "texture_frame.h"
 #include "lz4.h"
 #include "fb.h"
 
 void main() {
 	gsinit();
-	border(5);
+	border(7);
 
-	lz4_unpack(fbData, frame_data);
-	memset((unsigned char *)0x5800, 5 << 3, 0x300);
-	lz4_unpack(fbAttr, frame_attrs);
+	lz4_unpack(fbData, phone_data);
+	memset((unsigned char *)0x5800, 7 << 3, 0x300);
+	lz4_unpack(fbAttr, phone_attrs);
 	fb_update();
 
 	halt();
