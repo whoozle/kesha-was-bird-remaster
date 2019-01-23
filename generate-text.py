78f907b00521f9c126f58599086e8f1d9329fcdb
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-01-23 23:00:42 +0000

    updated generate-text from kesha was bIIrd

--- generate-text.py
+++ generate-text.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import argparse
 import os.path
@@ -12,6 +12,7 @@ args = parser.parse_args()
 
 offsets = []
 data = []
+lookup = {}
 
 addr = int(args.address, 16)
 header = """\
@@ -19,16 +20,39 @@ header = """\
 :const data_text_lo 0x%02x
 """ %(addr >> 8, addr & 0xff)
 
+def add(key, value):
+	global header, offsets, data, lookup
+
+	if value in lookup:
+		index = lookup[value]
+		header += ":const text_%s %d\n" %(key, index)
+		return
+
+	index, offset = len(offsets), len(data)
+	lookup[value] = index
+	header += ":const text_%s %d\n" %(key, index)
+	offsets.append(offset)
+	for ch in value:
+		if ch == '\n':
+			v = 0xff
+		else:
+			v = ord(ch) - 31
+		if v < 0:
+			raise Exception('invalid character %s' %repr(ch))
+		data.append(v)
+	data.append(0)
+
 for source in args.sources:
 	messages = json.load(open(source))
 	for key, value in messages.iteritems():
-		header += ":const text_%s %d\n" %(key, len(offsets))
-		offsets.append(len(data))
-		for ch in value:
-			data.append(ord(ch) - 31)
-		data.append(0)
+		if isinstance(value, list):
+			for idx, text in enumerate(value, 1):
+				add('%s_%d' %(key, idx), text)
+		else:
+			add(key, value)
 
-source = ":org 0x%04x\n" %(addr)
+source  = "#text size %u + %u = %u\n" %(len(data), len(offsets) * 2, len(data) + len(offsets) * 2)
+source += ":org 0x%04x\n" %(addr)
 source += ": data_text\n\t "
 source += " ".join(["0x%02x" %i for i in data])
 source += "\n\n"
