838ecf84406df099d14df56d754741be0b7ad527
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-01-23 22:58:20 +0000

    converted font generation

--- generate-font.py
+++ generate-font.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
-def generate(name, file, addr, font_height = 5, space_width = 3):
-	source = ":org 0x%04x\n" %addr
-	source += ": font_%s_data\n" %name
+def generate(name, file, font_height = 5, space_width = 3):
 	with open(file) as fontsource:
 		font = {}
 		while True:
@@ -20,112 +18,65 @@ def generate(name, file, addr, font_height = 5, space_width = 3):
 			else:
 				char, height, width, descent = (t(s) for t,s in zip((str, int, int, int), decl))
 
-			width += 1 #shadow + interval
 			if width > 8:
 				raise "width is more than 8"
 
-			rows, shadows = [], []
+			rows = []
 			for y in xrange(0, height):
 				rowstr = fontsource.readline().rstrip()
 				row = [1 if ch != ' ' else 0 for ch in rowstr]
 				for x in xrange(len(row), width):
 					row.append(0)
 
-				#shadow
-				shadow = [0 for i in xrange(0, width)]
-				for i in xrange(1, width):
-					x = width - 1 - i
-					if row[x] and not row[x + 1]:
-						shadow[x + 1] = 1
 				#print y, row, shadow
 				rows.append(row)
-				shadows.append(shadow)
 
-			font[char] = (height, width, descent, rows, shadows)
+			font[char] = (height, width, descent, rows)
 
 		#print font
 		chars = [ord(x) - 31 for x in font.keys()]
 		cmin, cmax = min(chars), max(chars)
 
 		glyph = 0
-		font_data = []
 		index_source = ""
 		for ch in xrange(cmin, cmax + 1):
 			key = chr(ch + 31)
 			if key in font:
-				height, width, descent, rows, shadow = font[key]
-				offset = len(font_data)
+				height, width, descent, rows = font[key]
+				glyph_data = []
 				assert height == len(rows)
-				assert height == len(shadow)
-				for data in [rows, shadow]:
-					for row in data:
-						value = 0
-						for i in xrange(0, width):
-							if row[i]:
-								value |= (0x80 >> i)
-						font_data.append(value)
+				for row in rows:
+					value = 0
+					for i in xrange(0, width):
+						if row[i]:
+							value |= (0x80 >> i)
+					glyph_data.append(value)
 
-				index_source += "%d 0x%02x 0x%02x %d %d " %(height, offset & 0xff, offset >> 8, width, (256 - descent) & 0xff)
+				index_source += "\t{ %d, %d, %d, { %s } },\n" %(width, height, (256 - descent) & 0xff, ", ".join(["0x%02x" %x for x in glyph_data]))
 				#print key, font[key]
 				glyph += 1
 			else:
-				index_source += "0 0xff 0xff %d 0 " %space_width
-	source += " ".join(["0x%02x" %x for x in font_data])
+				index_source += "\t{ %d, 0, 0 },\n" %space_width
+	source = '#include "font_%s.h"\n\n' %name
+	source += "Glyph font_%s[%d] =\n{\n" %(name, cmax - cmin + 1)
+	source += index_source
+	source += "\n};\n"
 	source += "\n\n"
-	source += ": data_%s_index\n%s\n\n" %(name, index_source)
-	source += "#glyphs: %d, size: %d\n\n" %(glyph, len(font_data))
-	header = """
-: draw_{name}_char_error
-	v0 := {space_width}
-	return
 
-: draw_font_char
-	vf := 31
-	vc -= vf
+	header = """\
+#ifndef FONT_{uname}_H
+#define FONT_{uname}_H
+#include "font.h"
 
-: _draw_{name}_char
-	if vc < {min} then jump draw_{name}_char_error
-	if vc > {max} then jump draw_{name}_char_error
+#define FONT_{uname}_SPACE_WIDTH ({space_width})
+#define FONT_{uname}_CMIN ({min})
+#define FONT_{uname}_CMAX ({max})
+#define FONT_{uname}_GLYPHS ({glyphs})
 
-	vc += -{min}
-	v0 := vc
-	v0 += vc
-	i := long data_font_index
-	i += v0
-	i += v0
-	i += vc
-	load v4 #v0 height v1 v2 offset v3 width v4 ascend
-	if v1 == -1 then jump draw_{name}_char_error
+extern Glyph font_{name}[{glyphs}];
 
-	i := draw_{name}_char_sprite_index
-	ve := 0xb0
-	v0 |= ve
-	save v0
-
-	v5 := v1
-	v6 := v2
-
-	v0 := {addrh}
-	v1 := {addrl}
-	v1 += v5
-	v0 += vf
-	v0 += v6
-	i := draw_{name}_load_glyph_addr
-	save v1
-
-	0xf0 0x00
-: draw_{name}_load_glyph_addr
-	0x00 0x00
-
-	vb += v4
-
-:next draw_{name}_char_sprite_index
-	sprite va vb 0
-	vb -= v4
-	v0 := v3
-	return
-
-""".format(min = cmin, max = cmax, name = name, space_width = space_width, addrh = addr >> 8, addrl = addr & 0xff)
+#endif
+""".format(min = cmin, max = cmax, glyphs = (cmax - cmin + 1), name = name, uname=name.upper(), space_width = space_width)
 	return header, source
 
 import argparse
@@ -134,15 +85,12 @@ import os.path
 parser = argparse.ArgumentParser(description='Compile font.')
 parser.add_argument('source', help='input file')
 parser.add_argument('name', help='input file')
-parser.add_argument('segment', help='font data address in high mem (e.g. f000)')
 parser.add_argument('target', help='target directory')
 args = parser.parse_args()
-addr = int(args.segment, 16)
 
-decl, defn = generate(args.name, args.source, addr)
-with open(os.path.join(args.target, args.name + ".8o"), "w") as f:
+decl, defn = generate(args.name, args.source)
+with open(os.path.join(args.target, "font_%s.h" %args.name), "wt") as f:
 	f.write(decl)
 
-with open(os.path.join(args.target, args.name + "_data.8o"), "w") as f:
+with open(os.path.join(args.target, "font_%s.c" %args.name), "wt") as f:
 	f.write(defn)
-
---
+++ sources/font.h
@@ -0,0 +1,14 @@
+#ifndef KESHA_FONT_H
+#define KESHA_FONT_H
+
+#include "types.h"
+
+struct Glyph
+{
+	u8 width, height;
+	u8 descend;
+	u8 data[];
+};
+typedef struct Glyph Glyph;
+
+#endif
