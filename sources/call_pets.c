752f52923501dff8447017b3d5c881798189344b
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-01-28 21:14:35 +0000

    added pets logic

--- sources/call_pets.c
+++ sources/call_pets.c
@@ -1,6 +1,21 @@
 #include "calls.h"
+#include "vars.h"
+#include "_dialogs.h"
 
 void call_pets(void)
 {
-
+	if (!pets_plan)
+	{
+		dialog_pets_1();
+	}
+	else
+	{
+		if (!pets_got_galina)
+		{
+			set_flag(&pets_called);
+			dialog_pets_2();
+		}
+		else
+			dialog_pets_3();
+	}
 }
