2abbcd8594d6866d45fe5520b74f8f2471f2db10
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-02-06 23:47:43 +0000

    implemented lab puzzle

--- sources/call_lab.c
+++ sources/call_lab.c
@@ -4,6 +4,8 @@
 #include "phone.h"
 #include "font.h"
 #include "_dialogs.h"
+#include "audio.h"
+#include "types.h"
 
 void ninja_fight(void);
 
@@ -13,13 +15,15 @@ inline static u8 rand_digit(u8 index)
 {
 	while(1)
 	{
-		u8 d = rand() & 0x0fu;
+		u8 d;
+	again:
+		d = rand() & 0x0fu;
 		if (d > 9)
 			d -= 6;
 
 		for (u8 i = 0; i < index; ++i)
 			if (pin[i] == d)
-				continue;
+				goto again;
 		return d;
 	}
 }
@@ -43,19 +47,46 @@ void call_lab(void)
 #endif
 	}
 
-	dialog_lab_security_1();
-
 	while(1)
 	{
-		for(u8 i = 0; i < 4; ++i)
+		dialog_lab_security_1();
+
+		u8 i, j, digit;
+
+		u8 bulls = 0, cows = 0;
+
+		for(i = 0; i < 4; ++i)
 		{
-			u8 digit = read_digit();
+			digit = read_digit();
+
 			if (digit > 9)
 				return;
-			font_draw_char(11 /* * */, 10 + (i << 3), 40);
+
+			if (pin[i] == digit)
+				++bulls;
+
+			for(j = 0; j < 4; ++j)
+				if (pin[j] == digit)
+					++cows;
+
+			font_draw_char(11 /* '*' */, 10 + (i << 3), 40);
+		}
+
+		if (bulls == 4)
+			return ninja_fight();
+
+		cows -= bulls;
+
+		u8 x = 10;
+		for(i = 0; i < bulls; ++i, x += 6)
+		{
+			font_draw_char(12, x, 50);
+			audio_play_sync_n(audio_lab_bull, 20);
+		}
+		for(i = 0; i < cows; ++i, x += 6)
+		{
+			font_draw_char(32, x, 50);
+			audio_play_sync_n(audio_lab_cow, 20);
 		}
 	}
-
-	u8 bulls = 0, cows = 0;
-
 }
