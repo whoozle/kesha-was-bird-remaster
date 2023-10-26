977dba0fe47290e752148d307b1db78e10fd7eb8
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2023-10-26 21:23:05 +0100

    use homemade copy because it seems that sdcc does memmove?

--- sources/lz4.c
+++ sources/lz4.c
@@ -1,6 +1,11 @@
 #include "lz4.h"
 #include <string.h>
 
+static void copy(const u8 *src, u8 *dst, u16 size) {
+    while(size--)
+        *dst++ = *src++;
+}
+
 const unsigned char * lz4_unpack(unsigned char *dst, const unsigned char *src)
 {
 	u16 usize = *(const u16 *)src;
@@ -21,13 +26,13 @@ const unsigned char * lz4_unpack(unsigned char *dst, const unsigned char *src)
 				}
 				while(next == 255);
 			}
-			memcpy(dst, src, size);
+			copy(src, dst, size);
 			dst += size;
 			src += size;
 		}
 
-		u16 offset = *src++;
-		offset |= ((u16)*src++) << 8;
+		u16 offset = *(const u16 *)src;
+		src += 2;
 
 		size = 4 + (code & 0x0f);
 		if (size == 19)
@@ -40,7 +45,7 @@ const unsigned char * lz4_unpack(unsigned char *dst, const unsigned char *src)
 			}
 			while(next == 255);
 		}
-		memcpy(dst, dst - offset, size);
+		copy(dst - offset, dst, size);
 		dst += size;
 	}
 
